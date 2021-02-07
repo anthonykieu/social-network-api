dbb3ace15c449cd14b4d49d2c24f29f9263c3736
Anthony Kieu <anthonykieu@gmail.com>
2021-02-07 13:58:07 -0500

    fixed some bugs in delete reactions

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -104,7 +104,7 @@ const thoughtController = {
     removeReaction({ params }, res) {
         Thought.findOneAndUpdate(
             { _id: params.thoughtId },
-            { $pull: { reactions: params.reactionId } },
+            { $pull: { reactions: {reactionId: params.reactionId }} },
             { new: true }
         )
             .then(dbThoughtData => {
